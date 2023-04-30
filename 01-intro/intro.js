9a368bc4e57e95c8902a80b15159de16f9c49ceb
saif-paywell <bd.saif321@gmail.com>
2023-04-30 16:04:02 +0600

    Intro on typescript and first play

---
+++ 01-intro/intro.js
@@ -0,0 +1,3 @@
+var user = { name: "Saiful", age: 30 };
+// console.log(user.email) //showing error instantly but compiled js code
+console.log(user.name);
---
+++ 01-intro/intro.ts
@@ -0,0 +1,5 @@
+let user = {name: "Saiful", age: 30}
+
+
+// console.log(user.email) //showing error instantly but compiled js code
+console.log(user.name)
