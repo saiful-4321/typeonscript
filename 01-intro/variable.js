b338c98049d045dcbfdd20166508295890c6bac7
saif-paywell <bd.saif321@gmail.com>
2023-05-02 12:31:58 +0600

    number and boolean discussed

--- 01-intro/variable.js
+++ 01-intro/variable.js
@@ -1,6 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// Premitive data types
+// String
 var greetings = "Hello world";
 var number = 6;
-number = "saif";
+// number = "saif" //can not assign wrong datatype
+greetings.toLowerCase();
 console.log(greetings);
+// Number
+var userId = 19008;
+number.toFixed();
+// boolean
+var isLoggedIn = true;
