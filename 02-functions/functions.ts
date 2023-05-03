aa002548a241415a957f182779baa21f4b37d4ad
saif-paywell <bd.saif321@gmail.com>
2023-05-03 23:52:42 +0600

    basic function

---
+++ 02-functions/functions.ts
@@ -0,0 +1,15 @@
+function addTwo(num: number) {
+    return num + 2
+}
+addTwo(5)
+
+function toUpper(str: string) {
+    return str.toUpperCase()
+}
+
+function signupUser(name: string, email: string, age: number, isValid: boolean) { }
+let loginUser = (email: string, password: string, isPaid: boolean = false) => {}
+
+signupUser("saiful", "saiful@gmail.com", 30, false)
+loginUser("saif@gmail.com", "1234")
+export{}
