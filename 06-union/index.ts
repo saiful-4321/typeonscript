5f5150e3900905d63bde89cb09f5d44586a690fe
saif-paywell <bd.saif321@gmail.com>
2023-05-06 22:58:55 +0600

    all about tuples types

--- 06-union/index.ts
+++ 06-union/index.ts
@@ -1,8 +1,7 @@
-let score: number | string = 44
-
-score = 55
-
-score = "fourth"
+// Multiple datatype assigning
+let result: number | string = 3.49
+result = 55
+result = "fourth"
 
 type User = {
     name: string,
@@ -14,19 +13,23 @@ type Admin = {
     id: number
 }
 
+// Multiple type assigning
 let userType: User | Admin = {name: "saiful", id: 4455}
 
+// we can use different type aswell
 userType = {username: "admin123", id: 887}
 
+// define multiple type while taking params 
 function getId(id: number | string) {
     console.log(`DB id is ${id}`)
 
+    // doing type specific functionalities
     if(typeof id === "string") {
         id.toLowerCase()
     }
 
     if(typeof id === "number") {
-        id+2
+        id.toString()
     }
 }
 
@@ -38,7 +41,7 @@ getId("3")
 // const data2: string[] = [1,2,3,"4"] //number not allowed
 const data3: (string | number | boolean)[] = [1,2,3,"4", true] //multiple type define
 
-let seatAllotment: "aisle" | "middle" | "window"
+let position: "aisle" | "middle" | "window"
 
-seatAllotment = "middle"
-// seatAllotment = "crew" //this is not acceptable beacause type is not defined
+position = "middle"
+// position = "crew" //this is not acceptable beacause type is not defined
