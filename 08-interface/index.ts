888d10ac40907c6fb328957082c34e79e02b18ca
saif-paywell <bd.saif321@gmail.com>
2023-05-07 13:18:19 +0600

    more about interfaces

--- 08-interface/index.ts
+++ 08-interface/index.ts
@@ -9,11 +9,29 @@ interface User {
 }
 
 
-
+interface User {
+    githubId: string
+}
 
 const user: User = {
     id: 1, email: "saiful@gmail.com", 
-    userId: 221, 
+    userId: 221,
+    githubId: "github",
+    startTrial: () => {return "Trial started"},
+    getCupon(name: "tenonten", value: 100) {
+        return 100
+    },
+}
+
+interface Admin extends User {
+    role: "admin" | "superadmin" | "learner"
+}
+
+const adminUser: Admin = {
+    id: 1, email: "saiful@gmail.com", 
+    userId: 221,
+    role: "admin",
+    githubId: "github",
     startTrial: () => {return "Trial started"},
     getCupon(name: "tenonten", value: 100) {
         return 100
