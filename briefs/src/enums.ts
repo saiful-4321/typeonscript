ddc81adb2f82f4a9218399bb58d09a6644e93d8c
saif-paywell <bd.saif321@gmail.com>
2023-05-20 03:50:04 +0600

    all about enum ty[e

---
+++ briefs/src/enums.ts
@@ -0,0 +1,11 @@
+enum Weekdays {
+    Monday = "Mon",
+    Tuesday = "Tue",
+    Wednesday = "Wed",
+    Thursday = "Thu",
+    Friday = "Fri",
+    Saturday = "Sat",
+    Sunday = "Sun"
+}
+
+console.log(Weekdays.Monday);
