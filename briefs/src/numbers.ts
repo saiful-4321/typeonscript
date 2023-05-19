50ae0bd12c1308d9ae0e8a4c240317c41140e74e
saif-paywell <bd.saif321@gmail.com>
2023-05-20 02:49:07 +0600

    all about numbers

---
+++ briefs/src/numbers.ts
@@ -0,0 +1,9 @@
+let value = 1000
+let another = new Number("1000")
+
+
+console.log(another)
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+console.log(Number.POSITIVE_INFINITY)
+console.log(Number.NaN)
